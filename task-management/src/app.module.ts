1e2abfae4333d5e3532c95ce3a0ad78840658e22
Fahad Hossain <fahadbin8900@gmail.com>
2024-03-28 12:23:24 +0600

    Database connected with user module and CURD operation

--- task-management/src/app.module.ts
+++ task-management/src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 @Module({
-  imports: [TasksModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'users',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
